28b0845460c8a1649c9460e38293f69e3cc1c3cb
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-12 20:23:18 +0300

    added new file

---
+++ 17-18-19.js
@@ -0,0 +1,28 @@
+17; // Only change code below this line
+
+var remainder;
+remainder = 11 % 3;
+
+18;
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+
+a += 12;
+b += 9;
+c += 7;
+
+19;
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only change code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
