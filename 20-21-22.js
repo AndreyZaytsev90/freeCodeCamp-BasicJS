519d88d06d52e96e093c147a4307cd2ed389ad7e
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-12 20:52:13 +0300

    added new file

---
+++ 20-21-22.js
@@ -0,0 +1,26 @@
+20;
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+21;
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+22;
+
+var myFirstName = "Andrey";
+var myLastName = "Zaytsev";
