ec624cf3093deee70d0ad671094a2b0295a57164
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-14 21:56:13 +0300

    added new file

--- 30-31-32.js
+++ 30-31-32.js
@@ -16,3 +16,12 @@ var lastName = "Lovelace";
 
 // Only change code below this line
 firstLetterOfLastName = lastName[0]; // Change this line
+
+32;
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
