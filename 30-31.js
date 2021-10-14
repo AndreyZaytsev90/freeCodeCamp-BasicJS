623623e8d40080036a4aa5b14c6e8aa8b689847b
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-14 21:43:02 +0300

    added new file

---
+++ 30-31.js
@@ -0,0 +1,18 @@
+30;
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line
+
+lastNameLength = lastName.length;
+console.log(lastNameLength);
+
+31;
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
