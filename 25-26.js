b2ecf14319fc27c0b1d9e7babfa717c1c4302a58
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-14 20:26:35 +0300

    added new file

--- 25-26.js
+++ 25-26.js
@@ -3,3 +3,9 @@ var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
 
 26;
 var myStr = "This is the start." + " This is the end."; // Change this line
+
+27;
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
