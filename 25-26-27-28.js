a7a26f981b74d0f6e74fe873cb8f902d5ce4e6f4
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-14 20:31:52 +0300

    added new file

--- 25-26-27-28.js
+++ 25-26-27-28.js
@@ -9,3 +9,8 @@ var myStr = "This is the start." + " This is the end."; // Change this line
 
 var myStr = "This is the first sentence. ";
 myStr += "This is the second sentence.";
+
+28;
+// Only change code below this line
+var myName = "Andrey";
+var myStr = "My name is " + myName + ", and I am well!";
