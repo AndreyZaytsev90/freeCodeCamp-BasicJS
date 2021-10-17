3c1a53a811862540dcd46d51cd7bb14ab024066e
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-17 21:05:03 +0300

    added new file

--- 37-38-39-40.js
+++ 37-38-39-40.js
@@ -9,3 +9,9 @@ var myArray = [["Andrey"], [31]];
 39;
 var myArray = [50, 60, 70];
 var myData = myArray[0];
+
+40;
+// Setup
+var myArray = [18, 64, 99];
+myArray[0] = 45;
+// Only change code below this line
