328423e9cc128a190d92b0d8b003a06997bbc06e
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-18 20:32:25 +0300

    added new file

---
+++ 41-42-43.js
@@ -0,0 +1,38 @@
+var myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+var myData = myArray[2][1];
+
+42;
+// Setup
+var myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
+myArray.push(["dog", 3]);
+// Only change code below this line
+
+43;
+
+// Setup
+var myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
+
+// Only change code below this line
+var removedFromMyArray = myArray.pop();
+
+44;
+// Setup
+var myArray = [
+  ["John", 23],
+  ["dog", 3],
+];
+
+// Only change code below this line
+var removedFromMyArray = myArray.shift();
