0427cbc846e70dbba2d256aba324105f05c912a3
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-20 21:12:09 +0300

    added new file

---
+++ 47-48.js
@@ -0,0 +1,11 @@
+47;
+function reusableFunction() {
+  console.log("Hi World");
+}
+console.log(reusableFunction());
+
+48;
+function functionWithArgs(a, b) {
+  console.log(a + b);
+}
+console.log(functionWithArgs(1, 2));
