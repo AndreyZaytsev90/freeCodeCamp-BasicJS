addf595caa4957ed938e8259579f0d1c3c479648
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-20 20:39:27 +0300

    added new file

--- 45.js
+++ 45.js
@@ -7,3 +7,12 @@ var myArray = [
 myArray.shift();
 myArray.unshift(["Paul", 35]);
 // Only change code below this line
+
+46;
+var myList = [
+  ["tea", 1],
+  ["bread", 2],
+  ["cheese", 3],
+  ["oil", 3],
+  ["milk", 2],
+];
