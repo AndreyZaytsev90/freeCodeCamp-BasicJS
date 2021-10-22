8335c882b322dd154477fdb3634a632bfb818cad
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-22 18:32:39 +0300

    added new file

--- 50-51-52.js
+++ 50-51-52.js
@@ -9,3 +9,24 @@ myLocalScope();
 // Run and check the console
 // myVar is not defined outside of myLocalScope
 console.log("outside myLocalScope", myVar);
+
+51;
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+
+  var outerWear = "sweater";
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
+52;
+
+function timesFive(x) {
+  return x * 5;
+}
