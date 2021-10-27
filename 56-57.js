a6ab75d379f5363670482cc706192e6ef9fd9e79
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-27 22:20:22 +0300

    added new file

--- 56-57.js
+++ 56-57.js
@@ -1,7 +1,18 @@
+56;
+
 function welcomeToBooleans() {
   // Only change code below this line
 
   return true; // Change this line
 
   // Only change code above this line
+}
+
+57;
+
+function trueOrFalse(wasThatTrue) {
+  if (wasThatTrue) {
+    return "Yes, that was true";
+  }
+  return "No, that was false";
 }
