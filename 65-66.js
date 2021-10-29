837855f8c5fc1bc8210141fd32a9e48bd6d8eb07
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-29 18:38:18 +0300

    added new file

---
+++ 65-66.js
@@ -0,0 +1,35 @@
+65;
+
+function testGreaterThan(val) {
+  if (val > 100) {
+    // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {
+    // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
+
+66;
+
+function testGreaterOrEqual(val) {
+  if (val >= 20) {
+    // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {
+    // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
