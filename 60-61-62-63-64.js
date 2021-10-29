b5384edd12f514ee0773f684f898acd9eec6a2ef
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-29 18:37:19 +0300

    added new file

--- 60-61-62-63-64.js
+++ 60-61-62-63-64.js
@@ -37,3 +37,16 @@ function testNotEqual(val) {
 }
 
 testNotEqual(10);
+
+64;
+
+// Setup
+function testStrictNotEqual(val) {
+  if (val !== 17) {
+    // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
