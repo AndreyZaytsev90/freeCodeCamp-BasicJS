bf125b785d7981193b914ce25d6ae6d2c63fab11
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-30 20:45:17 +0300

    added new file

---
+++ 69.js
@@ -0,0 +1,14 @@
+69;
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val >= 25 && val <= 50) {
+    return "Yes";
+  }
+  return "No";
+}
+
+// Only change code above this line
+
+testLogicalAnd(10);
