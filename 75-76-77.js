0e38955b13f295a5f913166db65fd154ea2ec250
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-01 22:00:10 +0300

    added new file

---
+++ 75-76-77.js
@@ -0,0 +1,50 @@
+75;
+
+76;
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      return "alpha";
+      break;
+    case 2:
+      return "beta";
+      break;
+    case 3:
+      return "gamma";
+      break;
+    case 4:
+      return "delta";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+
+77;
+
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "a":
+      return "apple";
+      break;
+    case "b":
+      return "bird";
+      break;
+    case "c":
+      return "cat";
+      break;
+    default:
+      return "stuff";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
