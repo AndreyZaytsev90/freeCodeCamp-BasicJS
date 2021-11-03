5298c7b1ac18d266b5601c41bd9f30560e840744
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-03 20:27:27 +0300

    added new file

---
+++ 84-85.js
@@ -0,0 +1,24 @@
+84;
+// Setup
+const testObj = {
+  hat: "ballcap",
+  shirt: "jersey",
+  shoes: "cleats",
+};
+
+// Only change code below this line
+const hatValue = testObj.hat; // Change this line
+const shirtValue = testObj.shirt; // Change this line
+
+85;
+
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water",
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"]; // Change this line
+const drinkValue = testObj["the drink"]; // Change this line
