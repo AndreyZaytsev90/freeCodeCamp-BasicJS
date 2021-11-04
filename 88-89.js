ccf3c19309d91dc210064112278d257e57881c36
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-04 21:11:55 +0300

    added new file

---
+++ 88-89.js
@@ -0,0 +1,22 @@
+88;
+
+const myDog = {
+  name: "Happy Coder",
+  legs: 4,
+  tails: 1,
+  friends: ["freeCodeCamp Campers"],
+};
+myDog.bark = "BOW!";
+
+89;
+
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+delete myDog.tails
+// Only change code below this line
