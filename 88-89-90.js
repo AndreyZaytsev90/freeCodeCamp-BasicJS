0ce9eef0010abeb15d8ffcb62e4545a82c4b304d
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-04 21:35:23 +0300

    added new file

---
+++ 88-89-90.js
@@ -0,0 +1,45 @@
+88;
+
+const myDog = {
+  name: "Happy Coder",
+  legs: 4,
+  tails: 1,
+  friends: ["freeCodeCamp Campers"],
+};
+myDog.bark = "BOW!";
+
+89;
+
+// Setup
+const myDog = {
+  name: "Happy Coder",
+  legs: 4,
+  tails: 1,
+  friends: ["freeCodeCamp Campers"],
+  bark: "woof",
+};
+delete myDog.tails;
+// Only change code below this line
+
+90;
+
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+
+  // Only change code below this line
+  const lookup = {
+    alpha: "Adams",
+    bravo: "Boston",
+    charlie: "Chicago",
+    delta: "Denver",
+    echo: "Easy",
+    foxtrot: "Frank",
+  };
+
+  result = lookup[val];
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
