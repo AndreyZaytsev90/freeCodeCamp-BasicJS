27b6fc73485f1779f7fb45ac902195bcfa107433
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-05 23:32:54 +0300

    added new file

--- 92-93.js
+++ 92-93.js
@@ -14,3 +14,19 @@ const myMusic = [
     gold: true,
   },
 ];
+
+93;
+
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
