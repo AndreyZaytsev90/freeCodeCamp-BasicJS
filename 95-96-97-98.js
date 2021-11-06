ad85d9b7890d309463ed4511a3f8a422cd939ac2
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-06 23:36:33 +0300

    added new file

---
+++ 95-96-97-98.js
@@ -0,0 +1,32 @@
+95;
+// коллекция записей
+
+96;
+
+// Setup
+const myArray = [];
+let i = 5;
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
+
+// Only change code below this line
+
+97;
+
+// Setup
+const myArray = [];
+for (let i = 1; i < 6; i++) {
+  myArray.push(i);
+}
+// Only change code below this line
+
+98;
+
+// Setup
+const myArray = [];
+for (let i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
+// Only change code below this line
