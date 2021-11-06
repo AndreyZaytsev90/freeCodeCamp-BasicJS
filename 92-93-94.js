3137227e7a509304b9b92300f23ec65a7c626442
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-06 22:40:12 +0300

    added new file

--- 92-93-94.js
+++ 92-93-94.js
@@ -18,15 +18,30 @@ const myMusic = [
 93;
 
 const myStorage = {
-  "car": {
-    "inside": {
+  car: {
+    inside: {
       "glove box": "maps",
-      "passenger seat": "crumbs"
-     },
-    "outside": {
-      "trunk": "jack"
-    }
-  }
+      "passenger seat": "crumbs",
+    },
+    outside: {
+      trunk: "jack",
+    },
+  },
 };
 
 const gloveBoxContents = myStorage.car.inside["glove box"];
+
+94;
+
+const myPlants = [
+  {
+    type: "flowers",
+    list: ["rose", "tulip", "dandelion"],
+  },
+  {
+    type: "trees",
+    list: ["fir", "pine", "birch"],
+  },
+];
+
+const secondTree = myPlants[1].list[1];
