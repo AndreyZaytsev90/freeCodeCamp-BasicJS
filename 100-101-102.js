a21f1974f83c97c4174438831d1c9ee298668ead
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-09 22:31:36 +0300

    added new file

--- 100-101-102.js
+++ 100-101-102.js
@@ -31,3 +31,15 @@ multiplyAll([
   [3, 4],
   [5, 6, 7],
 ]); // = 5040
+
+102;
+
+// Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+do {
+  myArray.push(i);
+  i++;
+} while (i < 10);
