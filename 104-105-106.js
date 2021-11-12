9f4e834413ef106671802fcdf393f2214a90fc3c
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-12 23:49:35 +0300

    added new files

--- 104-105-106.js
+++ 104-105-106.js
@@ -10,3 +10,12 @@ function randomFraction() {
 
     // Only change code above this line
 }
+
+106;
+
+function randomWholeNum() {
+
+    // Only change code below this line
+
+    return Math.floor(Math.random() * 10);
+}
