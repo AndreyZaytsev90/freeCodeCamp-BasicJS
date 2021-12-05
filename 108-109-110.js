336fbca2d966f9440173d519a489343802d6712b
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-12-05 11:49:33 +0300

    change file

--- 108-109-110.js
+++ 108-109-110.js
@@ -7,3 +7,11 @@ function convertToInteger(str) {
 
 convertToInteger("56");
 
+109;
+
+function convertToInteger(str) {
+    const a = parseInt(str, 2)
+    return a
+}
+
+convertToInteger("10011");
